54cc1c80863f72f3fa491a100034032558270a4e
stuart <stuarth323@gmail.com>
2017-09-29 15:17:33 -0400

    removed the table and changed layout a bit

--- contribution/src/components/containers/Header.js
+++ contribution/src/components/containers/Header.js
@@ -1,6 +1,5 @@
 import React, { PureComponent }     from 'react';
 import Supply                   from '../header/Supply';
-import Price                    from '../header/Price';
 import Countdown                from '../header/Countdown';
 import Register                 from '../header/Register';
 import LandingVideo             from '../utils/LandingVideo';
@@ -12,22 +11,17 @@ export default class Header extends PureComponent {
                 <div className="container">
                     <div className="row pv-40">
                         <div className="col-md-7">
-                            <Countdown  history={this.props.history} />
-                        </div>
-                        <div className="col-md-5">
-                            <Price />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-7">
                             <LandingVideo />
                         </div>
                         <div className="col-md-5">
                             <Supply />
                         </div>
                     </div>
-                    <div className="row pb-40">                        
-                        <div className="col-md-5 col-md-offset-7">
+                    <div className="row pb-40">
+                        <div className="col-md-7">
+                            <Countdown  history={this.props.history} />
+                        </div>
+                        <div className="col-md-5">
                             <Register   history={this.props.history} />
                         </div>
                     </div>
