b0f0a6deb776e8e1449d94b05162796d0338997f
stuart <stuarth323@gmail.com>
2017-09-29 15:58:53 -0400

    updated for mobile

--- contribution/src/components/token-information/Icons.js
+++ contribution/src/components/token-information/Icons.js
@@ -7,17 +7,17 @@ export default class Icons extends PureComponent{
             <section id="icon-container">
                 <div className="container">
                     <div className="row center-text">
-                        <div className="col-sm-4">
+                        <div className="col-md-4">
                             <img src={Consensys} alt="Ease of use" />
                             <h3>Beautiful UI</h3>
                             <h3>Ease of Use</h3>
                         </div>
-                        <div className="col-sm-4">
+                        <div className="col-md-4">
                             <img src={Consensys} alt="Ease of use" />
                             <h3>Decentralized</h3>
                             <h3>Derivatives Trading</h3>
                         </div>
-                        <div className="col-sm-4">
+                        <div className="col-md-4">
                             <img src={Consensys} alt="Ease of use" />
                             <h3>Secure</h3>
                             <h3>Provable Audit</h3>
--- contribution/src/components/token-information/News.js
+++ contribution/src/components/token-information/News.js
@@ -10,10 +10,10 @@ export default class News extends PureComponent{
                 <div className="container">
                     <h2>News</h2>
                     <div className="row">
-                        <div className="col-sm-2">
+                        <div className="col-md-2">
                             <img src={bcn} alt="Blockchain News Logo" className="news-image" />
                         </div>
-                        <div className="col-sm-10">
+                        <div className="col-md-10">
                             <h3>Non-Custodial Derivatives Platform</h3>
                             <p>Leverj, a decentralized platform for cryptocurrency derivatives trading, which is supported by ConsenSys, the world’s largest Blockchain venture studio – plans to unveil its highly anticipated platform to traders interested in leveraged cryptocurrency trading.</p>
                             <p>The Seychelles-based project will enable users to place and manage positions on digital currencies, as well as futures on major equities indices and single stock futures, all in a decentralized way.</p>
@@ -21,20 +21,20 @@ export default class News extends PureComponent{
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-sm-2">
+                        <div className="col-md-2">
                             <img src={nasdaq} alt="Nasdaq Logo" className="news-image" />
                         </div>
-                        <div className="col-sm-10">
+                        <div className="col-md-10">
                             <h3>First Steps Toward Trustless Bitcoin Futures Exchange</h3>
                             <p>Coinpit is taking significant and innovative steps to offer a safer alternative to Bitcoin's custodial futures exchanges. Thanks to Bitcoin's multi-signature technology, users that store bitcoins on Coinpit remain in complete control of their funds. Even if the exchange is hacked, goes bankrupt, or disappears completely, customers can in most cases still retrieve their money.</p>
                             <a href="http://www.nasdaq.com/article/coinpit-takes-first-steps-toward-trustless-bitcoin-futures-exchange-cm679644" target="_blank" rel="noopener noreferrer">Read More</a>
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-sm-2">
+                        <div className="col-md-2">
                             <img src={merkle} alt="Merkle Logo" className="news-image" />
                         </div>
-                        <div className="col-sm-10">
+                        <div className="col-md-10">
                             <h3>Blockchain & Bitcoin Conference Prague 2016</h3>
                             <p>Coinpit is a futures exchange which offers futures contracts denominated in Bitcoin. Currently, they offer inverse USD/BTC contracts. More contracts will be added based on demand. Furthermore, Coinpit is a pure Bitcoin in/out exchange and does not deal with any fiat, which eliminates any exchange rate risk. When it comes to security, Coinpit uses multi-sig addresses where a user’s private key is never transmitted to the exchange.</p>
                             <a href="http://themerkle.com/new-conference-speaker-bharath-rao-founder-of-coinpit-inc/" target="_blank" rel="noopener noreferrer">Read More</a>
