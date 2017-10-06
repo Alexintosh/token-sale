a8d547911dbbe4a4614a62c06b2e533722a64558
stuart <stuarth323@gmail.com>
2017-10-06 19:13:29 -0400

    updated whitepaper page

--- contribution/src/components/about/Whitepaper.js
+++ contribution/src/components/about/Whitepaper.js
@@ -4,30 +4,49 @@ export default class Whitepaper extends PureComponent {
     render(){
         return (
             <section id="whitepaper" className="pt-100 h-50">
-                <h2 className="sub-header fs-50 pt-30"><span>WHITEPAPER</span></h2>
-                <h4 className="font-bold fs-20">SECURE/ TRANSPARENT/ DECENTRALIZED</h4>
-                <p>
-                    Leverj decentralizes the most desirable features of derivatives trading by implementing them in cryptocurrencies and eliminating points of friction. 
-                    With a tight focus on derivatives trading and the supporting ecosystem, Leverj has taken the approach of defining the product frst. 
-                    We have built a functioning exchange with a usable UI (user interface), decentralized identity, and provable audit. 
-                    We plan to decentralize the back-end and add ecosystem features that will enable large players to move into the cryptocurrency world.
-                </p>
-                <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer">
-                    <i className="fa fa-file-pdf-o pr-20"></i>
-                    Read Whitepaper
-                </a>
-                <h4 className="font-bold fs-20">CUSTODIAL DECENTRALIZATION/FRAUD-PROOFS</h4>
-                <p>
-                    While centralized exchanges are very responsive, they are highly susceptible to custodial risk. 
-                    Fully decentralized exchanges (DEX) suffer from high latency and high cost, making them impractical for high volume use. 
-                    Using the on-chain order books and order matching introduce a variety of performance and economic issues that make current DEX unusable. 
-                    Leverj resolves the trade-offs between centralized and decentralized options by decentralizing safety-critical functions such as custody and centralizing the speed-critical functions such as order matching. 
-                    Custodial decentralization is done cheaply taking into account blockchain limitations such as congestion and probabilistic finality.
-                </p>
-                <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer">
-                    <i className="fa fa-file-pdf-o pr-20"></i>
-                    Read Yellowpaper
-                </a>
+                <h2 className="sub-header fs-50 pt-30"><span>WHITEPAPER & OTHER INFO</span></h2>
+                <div className="row pv-20">
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            English Whitepaper
+                        </a>
+                    </div>
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="yellow-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            English Yellowpaper
+                        </a>
+                    </div>
+                </div>
+                <div className="row pv-20">
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            Language1 Whitepaper
+                        </a>
+                    </div>
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="yellow-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            Language1 Yellowpaper
+                        </a>
+                    </div>
+                </div>
+                <div className="row pv-20">
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            Language2 Whitepaper
+                        </a>
+                    </div>
+                    <div className="col-sm-6">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="yellow-paper">
+                            <i className="fa fa-file-pdf-o pr-20"></i>
+                            Language2 Yellowpaper
+                        </a>
+                    </div>
+                </div>
             </section>
         )
     }
