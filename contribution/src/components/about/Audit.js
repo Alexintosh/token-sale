cba0ca5bd40098fa67be9603c1dae28c0ae5e74b
stuart <stuarth323@gmail.com>
2017-10-09 17:14:25 -0400

    added more style

--- contribution/src/components/about/Audit.js
+++ contribution/src/components/about/Audit.js
@@ -4,14 +4,14 @@ import diligence                from '../../public/img/diligence.png';
 export default class Audit extends PureComponent{
     render(){
         return(
-            <section id="audit" className="about-card h-50">
+            <section id="audit" className="about-card">
                 <div className="row">
                     <div className="col-sm-10">
                         <h2 className="sub-header-large"><span>CODE AUDIT</span></h2>
                         <p className="fs-30"><img src={diligence} alt="ConsenSys Diligence" className="diligence-img" />Consensys Diligence - Security Audit</p>
-                        <p>The code has been reviewed by ConsenSys Diligence. The Leverj Token Sale smart contract has been audited and tested. The contracts can be view on github.</p>
-                        <i className="fa fa-github fs-40" aria-hidden="true"></i>
-                        <a href="https://github.com/coinpit/token-sale" target="_blank" rel="noopener noreferrer" className="github">
+                        <p className="pb-20">The code is being reviewed by ConsenSys Diligence. The Leverj Token Sale smart contract is being audited and tested. The contracts can be viewed on github.</p>
+                        <a href="https://github.com/coinpit/token-sale" target="_blank" rel="noopener noreferrer" className="github pt-30">
+                            <i className="fa fa-github fs-40 pr-10" aria-hidden="true"></i>
                              View on GitHub
                         </a>
                     </div>
--- contribution/src/components/about/Whitepaper.js
+++ contribution/src/components/about/Whitepaper.js
@@ -5,68 +5,66 @@ import yellowpaper              from '../../public/img/leverj_yellow.png'
 export default class Whitepaper extends PureComponent {
     render(){
         return (
-            <section id="whitepaper" className="about-card h-50">
+            <section id="whitepaper" className="about-card">
                 <h2 className="sub-header-large"><span>WHITEPAPER & OTHER INFO</span></h2>
-                <div className="pt-40">
-                    <div className="row pv-20">
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ whitepaper } alt="Leverj Whitepaper" />
-                                English Whitepaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                English Yellowpaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                English Yellowpaper
-                            </a>
-                        </div>
+                <div className="row pv-20">
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ whitepaper } alt="Leverj Whitepaper" />
+                            English Whitepaper
+                        </a>
                     </div>
-                    <div className="row pv-20">
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ whitepaper } alt="Leverj Whitepaper" />
-                                Language1 Whitepaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                Language1 Yellowpaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                Language1 Yellowpaper
-                            </a>
-                        </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            English Yellowpaper
+                        </a>
                     </div>
-                    <div className="row pv-20">
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ whitepaper } alt="Leverj Whitepaper" />
-                                Language2 Whitepaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                Language2 Yellowpaper
-                            </a>
-                        </div>
-                        <div className="col-sm-4">
-                            <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
-                                <img src={ yellowpaper } alt="Leverj Yellowpaper" />
-                                Language2 Yellowpaper
-                            </a>
-                        </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            English Yellowpaper
+                        </a>
+                    </div>
+                </div>
+                <div className="row pv-20">
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ whitepaper } alt="Leverj Whitepaper" />
+                            Language1 Whitepaper
+                        </a>
+                    </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            Language1 Yellowpaper
+                        </a>
+                    </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            Language1 Yellowpaper
+                        </a>
+                    </div>
+                </div>
+                <div className="row pv-20">
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ whitepaper } alt="Leverj Whitepaper" />
+                            Language2 Whitepaper
+                        </a>
+                    </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            Language2 Yellowpaper
+                        </a>
+                    </div>
+                    <div className="col-sm-4">
+                        <a href="https://leverj.io/whitepaper.pdf" target="_blank" rel="noopener noreferrer" className="white-paper">
+                            <img src={ yellowpaper } alt="Leverj Yellowpaper" />
+                            Language2 Yellowpaper
+                        </a>
                     </div>
                 </div>
             </section>
