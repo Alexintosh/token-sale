5b80d768eb097ffc1b998d06002e97d3926d9b3e
stuart <stuarth323@gmail.com>
2017-10-10 08:55:57 -0400

    updated some styling and added more to whitelist

--- contribution/src/components/about/Whitelist.js
+++ contribution/src/components/about/Whitelist.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react';
 export default class Whitelist extends PureComponent{
     render(){
         return(
-            <section id="whitelist" className="pt-100 about-card">
+            <section id="whitelist" className="pt-100 about-card center-mobile-h">
                 <h2 className="sub-header-large"><span>WHITELIST</span></h2>
                 <p>
                     The purpose of the whitelist is to give Leverj supporters and followers early access to the public sale to ensure everyone gets a fair chance to participate. 
@@ -25,6 +25,14 @@ export default class Whitelist extends PureComponent{
                         Only ETH is accepted. Do no try sending anything other than ETH. *Always double check addresses you are sending ETH to.
                     </li>
                 </ul>
+                <p className="p-header">How to Register:</p>
+                <ol>
+                    <li>Click Register</li>
+                    <li>Provide All Required Information</li>
+                    <li>You will be sent an email confirming your registration (if successful)</li>
+                </ol>
+                <p className="p-header">How to Contribute</p>
+                <a href="#contribute">See More Info Below</a>
             </section>
         )
     }
