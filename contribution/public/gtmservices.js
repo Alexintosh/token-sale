faa0b567611d39a0f0c28a2dcb304a054c4f75d8
stuart <stuarth323@gmail.com>
2017-10-11 13:42:38 -0400

    forgot to add in

---
+++ contribution/public/gtmservices.js
@@ -0,0 +1,8 @@
+(function(){
+    var google_conversion_id = 840593584;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "CLTDCLafxXUQsOHpkAM";
+    var google_remarketing_only = false;
+})
