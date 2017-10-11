49a12db08ce53f0c4488139f81465d4b652e6b2b
stuart <stuarth323@gmail.com>
2017-10-11 11:53:27 -0400

    updated server

--- contribution/registration/server.js
+++ contribution/registration/server.js
@@ -204,7 +204,7 @@ app.post('/api/register', async (req, res) => {
             if(validator.isEmail(req.body.email)){
                 if(validator.isAlphanumeric(req.body.address) && req.body.address.length === 42){
                     if(validator.isAlphanumeric(req.body.country)){
-                        if(validator.isDecimal(req.body.amount) && req.body.amount >= 25 && req.body.amount <= 500){
+                        if(validator.isDecimal(req.body.amount) && req.body.amount >=1 && req.body.amount <= 500){
                                           
                                                 datastore_lib.addRegistration({
                                 name: req.body.name,
