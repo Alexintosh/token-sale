d4b7fb57026eb884d5edba7546378513ccc961b3
stuart <stuarth323@gmail.com>
2017-10-11 12:54:29 -0400

    js files

---
+++ contribution/public/gservices.js
@@ -0,0 +1,19 @@
+
+  <script>
+    <script type="text/javascript">
+    var google_conversion_id = 840593584;
+    var google_conversion_language = "en";
+    var google_conversion_format = "3";
+    var google_conversion_color = "ffffff";
+    var google_conversion_label = "CLTDCLafxXUQsOHpkAM";
+    var google_remarketing_only = false;
+    /* ]]> */
+    </script>
+    <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+    </script>
+    <noscript>
+    <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/840593584/?label=CLTDCLafxXUQsOHpkAM&amp;guid=ON&amp;script=0"/>
+    </div>
+    </noscript>  
+  </script>
