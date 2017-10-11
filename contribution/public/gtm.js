2e7e80a46344655e4366f8f09fe8e13a2f73c264
stuart <stuarth323@gmail.com>
2017-10-11 12:58:28 -0400

    updated file

--- contribution/public/gtm.js
+++ contribution/public/gtm.js
@@ -1,10 +1,10 @@
 <script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-91770964-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+  <script async src="/tagmanager.js"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
 
-  gtag('config', 'UA-91770964-1');
-</script>
+    gtag('config', 'UA-91770964-1');
+  </script>
 </script>    
