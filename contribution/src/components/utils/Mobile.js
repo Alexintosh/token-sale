38263e94881b3cf56d71a96c508edf0ca65e9a2e
stuart <stuarth323@gmail.com>
2017-10-13 12:31:13 -0400

    fixed error

--- contribution/src/components/utils/Mobile.js
+++ contribution/src/components/utils/Mobile.js
@@ -21,7 +21,7 @@ export default class Mobile extends Component{
                     <a id="over" className="menu-item" href="#overview" onClick={() => this.toggle()}>OVERVIEW</a>
                     <a id="info" className="menu-item" href="#information" onClick={() => this.toggle()}>TOKEN INFORMATION</a>
                     <a id="cont" className="menu-item" href="#contribute" onClick={() => this.toggle()}>HOW TO CONTRIBUTE</a>
-                    <a id="white" className="menu-item" href="whitepaper" onClick={() => this.toggle()}>WHITEPAPER</a>
+                    <a id="white" className="menu-item" href="#whitepaper" onClick={() => this.toggle()}>WHITEPAPER</a>
                     <a id="aud" className="menu-item" href="#audit" onClick={() => this.toggle()}>CODE AUDIT</a>
                     <a id="f" className="menu-item" href="#faq" onClick={() => this.toggle()}>FAQs</a>
                 </Menu>
