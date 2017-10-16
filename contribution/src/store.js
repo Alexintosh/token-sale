abc9607a0afa49ff23344fa2ceee30c6fae237ce
jsonsivar <jsonsivar@gmail.com>
2017-10-16 15:25:45 -0400

    updated store

--- contribution/src/store.js
+++ contribution/src/store.js
@@ -4,7 +4,7 @@ import { createLogicMiddleware }        from 'redux-logic';
 import arrLogic                         from './logic';
 
 const APIEndpoint = 'http://localhost:8080';
-//const APIEndpoint = 'https://api.leverj.test.tokenry.ca';  // TEST
+//const APIEndpoint = 'https://leverj-test-api.tokenry.io';  // TEST
 //const APIEndpoint = 'https://leverj-api.tokenry.io';  // PROD
 
 const logicMiddleware = createLogicMiddleware(arrLogic, { APIEndpoint });
