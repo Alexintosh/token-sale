b857e0bc217408f56e42184ab6eea2f67ad6ada8
stuart <stuarth323@gmail.com>
2017-10-18 13:08:37 -0400

    footer update

--- contribution/src/components/utils/Footer.js
+++ contribution/src/components/utils/Footer.js
@@ -41,6 +41,8 @@ export default class Footer extends PureComponent{
                                 </li>
                             </ul>
                             <p>© COPYRIGHT LEVERJ.IO 2017.</p>
+                            <p>Victoria, Seychelles</p>
+                            <p><a href="mailto:info@leverj.io">info@leverj.io</a></p>
                         </div>
                     </div>
                 </div>
